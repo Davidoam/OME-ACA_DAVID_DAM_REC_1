700a6f3d7cc6ba2abc35676a5f8a1416c618d5b7
David Omeñaca Ayuso <a24557@svalero.com>
2026-06-02 11:15:15 +0200

    Update Main.java

--- DAM2_OMENACA_DAVID_JDBC_2/DAM2_OMENACA_DAVID_JDBC_2/src/main/java/org/example/Main.java
+++ DAM2_OMENACA_DAVID_JDBC_2/DAM2_OMENACA_DAVID_JDBC_2/src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import beans.CentroForense;
+import beans.InformeForense;
+import beans.MuestraForense;
+import dao.MuestraForenseDAOImpl;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MuestraForenseDAOImpl muestraForenseDAO = new MuestraForenseDAOImpl();
+
+        System.out.println("===== TEST 1: ADD SATELLITE =====");
+
+        CentroForense centroForense = new CentroForense();
+        centroForense.setId(1);
+
+        MuestraForense muestraForense = new MuestraForense();
+        muestraForense.setCodigoCaso(100);
+        muestraForense.setTipoMuestra("Muestra-10");
+        muestraForense.setFechaRecogida(2020-11-11);
+        muestraForense.setEstadoCustodia("Custodiado");
+        muestraForense.setInforme(new InformeForense());
+
+
+        int insertados = muestraForenseDAO.add(muestraForense);
+        System.out.println("Filas insertadas: " + insertados);
+
+
+        muestraForenseDAO.findById(1);
+
+        muestraForenseDAO.findByAgencia(1);
+
+        muestraForenseDAO.findWithInforme(1);
+
+        System.out.println("===== TEST 4: FIND ALL Forenses =====");
+        ArrayList<MuestraForense> muestraForenses = (ArrayList<MuestraForense>) muestraForenseDAO.findAll();
+
+        for (MuestraForense muestraForense1 : muestraForenses) {
+            System.out.println(muestraForense1);
+        }
+
+        muestraForenseDAO.update(muestraForenses.set(1, muestraForense));
+        
     }
 }
