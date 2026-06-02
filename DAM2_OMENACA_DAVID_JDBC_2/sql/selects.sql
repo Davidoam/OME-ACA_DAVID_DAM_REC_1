84613f22e88b54ab0bfdbe838b3aa7d989b931b2
David Omeñaca Ayuso <a24557@svalero.com>
2026-06-02 11:16:13 +0200

    Update selects.sql

--- DAM2_OMENACA_DAVID_JDBC_2/sql/selects.sql
+++ DAM2_OMENACA_DAVID_JDBC_2/sql/selects.sql
@@ -4,26 +4,51 @@ SELECT * FROM MUESTRAS_FORENSES;
 
 SELECT * FROM INFORMES_FORENSES;
 
+
 SELECT 
     m.id AS muestra_id,
-    m.nombre AS muestra_nombre,
     m.codigo_caso,
     m.tipo_muestra,
-    m.fecha_lanzamiento,
+    m.fecha_recogida,
     m.estado_custodia,
 
     c.id AS centro_id,
-    c.nombre AS centro_nombre,
-    c.pais,
+    c.nombre AS agencia_nombre,
+    c.pais AS agencia_pais,
     c.nivel_seguridad,
-
-    i.id AS informes_id,
-    i.adn_positivo,
-    i.nivel_riesgo,
-    i.conclusion,
 
 FROM MUESTRAS_FORENSES m
 INNER JOIN CENTROS_FORENSES c
-    ON c.centro_id = c.id
+    ON m.centro_id = c.id;
+
+
+SELECT 
+    m.id AS muestra_id,
+    m.codigo_caso,
+    m.tipo_muestra,
+    m.fecha_recogida,
+    m.estado_custodia,
+
+    c.id AS centro_id,
+    c.nombre AS agencia_nombre,
+    c.pais AS agencia_pais,
+    c.nivel_seguridad,
+
+    i.id AS informe_id,
+    i.adn_positivo,
+    i.nivel_riesgo,
+    i.conclusion
+
+FROM MUESTRAS_FORENSES m
+INNER JOIN CENTROS_FORENSES c
+    ON m.centro_id = c.id
 INNER JOIN INFORMES_FORENSES i
-    ON m.muestra_id = m.id;
+    ON i.muestra_id = m.id;
+
+SELECT adn_positivo, nivel_riesgo, pais
+
+FROM INFORMES_FORENSES i
+INNER JOIN MUESTRAS_FORENSES 
+    ON i.muestra_id = m.id;
+INNER JOIN CENTROS_FORENSES c
+    ON c.centro_id = c.id;
