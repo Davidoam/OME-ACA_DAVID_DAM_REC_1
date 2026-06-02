5a9caf9fad2d12879932973f4319329867e03545
David Omeñaca Ayuso <a24557@svalero.com>
2026-06-02 11:27:55 +0200

    Update Main.java

--- DAM2_OMENACA_DAVID_JDBC_2/DAM2_OMENACA_DAVID_JDBC_2/src/main/java/DAM2_OMENACA_DAVID_JDBC_2/DAM2_OMENACA_DAVID_JDBC_2/src/main/java/org/example/Main.java
+++ DAM2_OMENACA_DAVID_JDBC_2/DAM2_OMENACA_DAVID_JDBC_2/src/main/java/DAM2_OMENACA_DAVID_JDBC_2/DAM2_OMENACA_DAVID_JDBC_2/src/main/java/org/example/Main.java
@@ -44,39 +44,7 @@ public class Main {
 
         muestraForenseDAO.update(muestraForenses.set(1, muestraForense));
 
+        muestraForenseDAO.updateDinamico(muestraForense);
 
-        /*
-
-        peliculaDAO.check();
-        // Prueba Unitaria: ADD Película
-        Pelicula pelicula = new Pelicula();
-        pelicula.setTitulo("300");
-        pelicula.setDirector("Zack Snyder");
-        pelicula.setAnyo(2007);
-        pelicula.setGenero("Cine Epico");
-        peliculaDAO.add(pelicula);
-        // Fin Prueba Unitaria: ADD Película
-        // Prueba Unitaria: LISTAR PELÍCULAS
-        ArrayList<Pelicula> lstPelicula = peliculaDAO.findAll();
-        for (Pelicula pelicula1:lstPelicula
-             ) {
-            System.out.println(pelicula1.toString());
-        }
-        // Fin Prueba Unitaria: LISTAR PELÍCULAS
-        // Prueba Unitaria: ELIMINAR
-            peliculaDAO.delete(9);
-        // Prueba Unitaria: FIN ELIMINAR
-        // Fin Prueba Unitaria: ELIMINAR
-
-        // Prueba Unitaria: FIND
-        peliculaDAO.find(2);
-        // Prueba Unitaria: FIND
-
-        // Prueba Unitaria: UPDATE
-        // peliculaDAO.update(¿?¿?¿?);
-        // Prueba Unitaria: UPDATE
-        // Prueba Unitaria: FIND DETALLE DE PEDIDO
-        peliculaDAO.findDetallePeliculaByPelicula(5);
-        // Prueba Unitaria:  FIND DETALLE DE PEDIDO*/
     }
 }
